105a6ee6ebabdf5604ec36b03d9370d480fb825e
raphael <raphael.huille@toucantoco.com>
2018-11-02 18:48:30 +0100

    first draft

--- script.js
+++ script.js
@@ -1,58 +1,107 @@
 var app = new Vue({
   el: '#app',
   data:{
-      columns:[{"name":"index", "display":true, "type":"Numeric", 
-                "Settings":{
-                    "Numeric":{"structure":"None", "min":0, "max":100000, "digit":0},
-                    "Label":{"structure":"Breakdown", "labelsList":[], "dateExtraction":"%m", "parentColumn":""},
-                    "Date":{"startDate":"", "endDate":"", "granularity":"Month", "step":1, "format":"%Y-%m-%d"},
-                },
-                }],
-      data:[[1], [2], [3]]
+    charts:[{
+        type:"Linechart",
+        settings:{
+            "value":{
+                isActive:true,
+                isMandatory:true,
+                type:"numeric",
+                settings:{
+                    "name":{value:"value", type:"text"}, 
+                    "min":{value:0, type:"number"},
+                    "max":{value:100, type:"number"},
+                    "structure":{value:"None", type:"select", options:["None", "YTD", "Percentage"]},
+                    "digits":{value:0, type:"number"},}, 
+            },
+            "date":{
+                isActive:true,
+                isMandatory:true,
+                settings:{
+                    "name":{value:"value", type:"text"}, 
+                    "start":{value:null, type:"date"},
+                    "end":{value:null, type:"date"},
+                    "granularity":{value:"Month", type:"select", options:["Day", "Month", "Year"]},
+                    "step":{value:1, type:"number"},}, 
+            },
+            "groups": {
+                isActive:false,
+                isMandatory:false,
+                settings:{
+                    "name":{value:"value", type:"text"}, 
+                    "values":{value:"GroupeA/GroupeB/GroupeC", type:"textarea"}, 
+                }
+            },
+            "filter-bottom-right": {
+                isActive:false,
+                isMandatory:false,
+                settings:{
+                    "name":{value:"value", type:"text"}, 
+                    "values":{value:"GroupeA/GroupeB/GroupeC", type:"textarea"}, 
+                }
+            },
+            "filter-upper-right": {
+                isActive:false,
+                isMandatory:false,
+                settings:{
+                    "name":{value:"value", type:"text"}, 
+                    "values":{value:"GroupeA/GroupeB/GroupeC", type:"textarea"}, 
+                }
+            },
+            "filter-upper-middle": {
+                isActive:false,
+                isMandatory:false,
+                settings:{
+                    "name":{value:"value", type:"text"}, 
+                    "values":{value:"GroupeA/GroupeB/GroupeC", type:"textarea"}, 
+                }
+            }
+        },
+    }],
+    activeType:"Linechart"
   },
 
   methods:{
-    addColumn:function(){
-        console.log("addColumn")
-        var defaultColumnSettings = {"name":"New column", "display":true, "type":"Numeric", 
-        "Settings":{
-            "Numeric":{"structure":"None", "min":0, "max":100, "digit":0},
-            "Label":{"structure":"Breakdown", "labelsList":[], "dateExtraction":"%m", "parentColumn":""},
-            "Date":{"startDate":"", "endDate":"", "granularity":"Month", "step":1, "format":"%Y-%m-%d"},},
-        }
+    generateAndDownload:function(){
+        console.log("generateAndDownload")
+        var settings = _.filter(app.charts, function(c){return c['type']==app.activeType})[0]['settings']
+        var fakir = app['generate'+app.activeType](settings)
+
+        csvContent = "data:text/csv;charset=utf-8,";
+        fakir.forEach(function(rowArray){
+        row = rowArray.join(",");
+        csvContent += row + "\r\n";
+        }); 
+
+        var encodedUri = encodeURI(csvContent);
+        var link = document.createElement("a")
+        link.setAttribute("href", encodedUri);
+        link.setAttribute("download", "fakir_data.csv");
+        link.click(); 
+    },
+
+    generateLinechart: function(settings){
         
-        _.map(app.columns, function(c){c.display =  false})
-        app.columns.push(defaultColumnSettings)
-    },
-    remove: function(index){
-        console.log(index)
-        app.columns.splice(index, 1)
-        _.map(app.data, function(row){row.splice(index, 1)})
-    },
-    displaySettings:function(column){
-        console.log("displaySettings")
-        _.map(_.filter(app.columns, function(c){return c != column}), function(c){c.display =  false})
-        column.display = !column.display
-    },
-    apply:function(){
-        console.log("apply")
+        var fakir;
+        labelsInputValue = 
+        fakir = product(labelsInputValue)
 
-        _.map(app.data, function(row){row.push(_.random())})
-
-
+        return fakir
     },
-    
+
+    product: function(args) {
+        return args.reduce(function tl (accumulator, value) {
+            var tmp = [];
+            accumulator.forEach(function (a0) {
+            value.forEach(function (a1) {
+                tmp.push(a0.concat(a1));
+            });
+            });
+            return tmp;
+        }, [[]]);
+    }
+
   }
 })
 
-
-// Date
-// Label
-    // Breakdown
-    // Date extraction
-    // Children of :
-
-// Numeric
-    // None
-    // YTD
-    // Percentage
